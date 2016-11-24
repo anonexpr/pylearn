5d099d909f089c290ea606e7a0184d13ac04b762
Murugan <ramaswamy.murugan@RINMAC108.local>
2016-11-24 09:28:25 +0530

    various operations on an array

---
+++ array_ops.py
@@ -0,0 +1,21 @@
+# General operations for an arry in python
+
+#Length of array
+print len([1,2,3,4,5])
+
+#Concatenate two arrays
+print([1,2,3] + [4,5,6])
+
+#Repetion
+print(["Hello"]*4)
+
+#check if an element is in array
+if 3 in [1,2,3]:
+   print "element exists"
+else:
+   print "element does not exist"
+
+
+#iterate an array
+for i in [1, 2, 3]:
+    print i
