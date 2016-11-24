f03a0c228e176579696d703ce64356df9c99c97e
Murugan <ramaswamy.murugan@RINMAC108.local>
2016-11-24 13:45:55 +0530

    what is lambda and how to use it

---
+++ lambda.py
@@ -0,0 +1,9 @@
+# lambda argument_list: expression 
+
+# argument list consists of a comma separated list of arguments andi, 
+# the expression is an arithmetic expression using these arguments
+
+sum = lambda x,y : x+y
+print type(sum)
+print sum(3,4)
+
